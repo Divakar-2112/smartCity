f5fc4466ca537ab7899fc35de33b5ca1dcaa0703
Divakar <divakarbdckap@gmail.com>
2025-05-20 12:19:14 +0530

    added database modeles

---
+++ citizen/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+from .models import *
+
+admin.site.register(Department)
+admin.site.register(SubCategory)
+admin.site.register(ComplaintDetail)
+admin.site.register(User)
+
+
+
+
