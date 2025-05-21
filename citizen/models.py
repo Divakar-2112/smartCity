2a27909603471dee8d8a6a51f7643673b08fbc7d
balajioffl <balajioffl07@gmail.com>
2025-05-21 17:20:42 +0530

    admin module added

--- citizen/models.py
+++ citizen/models.py
@@ -12,7 +12,7 @@ STATUS_CHOICES = [
     ('Resolved', 'Resolved'),
     ('Rejected', 'Rejected'),
 ]
-    
+
 class User(models.Model):
     user_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
@@ -33,7 +33,6 @@ class Department(models.Model):
     def __str__(self):
         return self.name
 
-
 class SubCategory(models.Model):
     subCategory_id = models.AutoField(primary_key=True)
     department = models.ForeignKey(Department, on_delete=models.CASCADE)
@@ -43,9 +42,9 @@ class SubCategory(models.Model):
     def __str__(self):
         return f"{self.name} ({self.department.name})"
 
-
 class ComplaintDetail(models.Model):
     complaint_id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     department = models.ForeignKey(Department, on_delete=models.CASCADE)
     subCategory = models.ForeignKey(SubCategory, on_delete=models.CASCADE)
     description = models.TextField()
@@ -56,4 +55,4 @@ class ComplaintDetail(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"Complaint #{self.complaint_id} by {self.name}"
+        return f"Complaint #{self.complaint_id} by {self.user.name}"
