bea30550c495e893b7272c1b95e5593c826c2316
Divakar <divakarbdckap@gmail.com>
2025-05-22 17:16:56 +0530

    pull request

--- department/views.py
+++ department/views.py
@@ -5,8 +5,10 @@ from citizen.models import *
 def staff(request):
     department=Department.objects.all()
     subCategory = SubCategory.objects.all()
+    complaints = ComplaintDetail.objects.all()
     context={
         "subCategory": subCategory,
-        "department":department
+        "department":department,
+        "complaints":complaints
     }
     return render(request, 'department/staff.html', context)
