5c4861ba0e7e1cfe7217fa6f04fb76f07ff5decd
balajioffl <balajioffl07@gmail.com>
2025-05-22 17:12:09 +0530

    register form added

--- citizen/views.py
+++ citizen/views.py
@@ -1,13 +1,34 @@
-from django.shortcuts import render
-from django.contrib import messages
+from django.shortcuts import render, redirect
+from django.contrib import messages 
 from .models import *
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from .form import CreateUserForm
+from django.contrib.auth.models import User
 
 def home(request):
     return render(request, "citizen/index.html")
 
 def register(request):
-    return render(request, "citizen/register.html")
+    if request.method == 'POST':
+        name = request.POST['username']
+        email = request.POST['email']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+
+        if password1==password2:
+            user = User.objects.create_user(username=name,email=email,password=password1)
+            user.save()
+            messages.success(request,'Your account has been created !')
+            return redirect('login')
+        else:
+            messages.warning(request,"Incorrect Password TRY AGAIN !!")
+            return redirect('register')
+
+ 
+    else:
+        form = CreateUserForm()
+        return render(request, "citizen/register.html",{'form':form})
 
 def login(request):
     return render(request, "citizen/login.html")
--- my_project/settings.py
+++ my_project/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'citizen',
     'department',
-    'myadmin'
+    'myadmin',
 ]
 
 MIDDLEWARE = [
@@ -131,6 +131,7 @@ MEDIA_ROOT = BASE_DIR/'static'
 STATICFILES_DIRS=[
     BASE_DIR/'static'
 ]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
