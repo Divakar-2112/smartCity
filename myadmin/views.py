0c08c38bd932ae11e325c48dcfd0517e3deda309
Lokeshwara19 <lokeshwarardckap@gmail.com>
2025-05-26 11:28:14 +0530

    pull request

--- myadmin/views.py
+++ myadmin/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
-
+from collections import defaultdict
+from citizen.models import *
 # Create your views here.
 
 def myadmin(request):
-    return render(request,"myadmin/myadmin.html")
+    department = Department.objects.all()
+    subCategory = SubCategory.objects.all()
+    complaints = ComplaintDetail.objects.select_related('department', 'user').all()
+
+    department_complaints = defaultdict(list)
+    for complaint in complaints:
+        department_complaints[complaint.department.name].append(complaint)
+
+    context = {
+        "subCategory": subCategory,
+        "department": department,
+        "department_complaints": dict(department_complaints),
+        "user": request.user  
+    }
+    return render(request,"myadmin/myadmin.html",context)
