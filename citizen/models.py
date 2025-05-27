f75315e2ab8eb29c86c757fde3c8a71a6b3b15db
balajioffl <balajioffl07@gmail.com>
2025-05-27 16:37:10 +0530

    pull request

--- citizen/models.py
+++ citizen/models.py
@@ -33,7 +33,7 @@ class NewUser(AbstractUser):
     department = models.ForeignKey('Department', on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
-        return self.username
+        return self.name
 
 class SubCategory(models.Model):
     subCategory_id = models.AutoField(primary_key=True)
--- my_project/settings.py
+++ my_project/settings.py
@@ -51,6 +51,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'my_project.middleware.restrict_user_access.RestrictUserAccessMiddleware',
+
 ]
 
 ROOT_URLCONF = 'my_project.urls'
