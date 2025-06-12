d47abff6b4b4547723f091203f93ffe169218a54
balajioffl <balajioffl07@gmail.com>
2025-06-12 10:11:30 +0530

    font styles refined

--- citizen/form.py
+++ citizen/form.py
@@ -3,11 +3,6 @@ from .models import NewUser
 from django.contrib.auth.forms import UserCreationForm
 
 class CreateUserForm(UserCreationForm):
-    password2 = forms.CharField(
-        label='Confirm Password',
-        widget=forms.PasswordInput
-    )
-
     class Meta:
         model = NewUser
         fields = ['username', 'email', 'password1', 'password2']
